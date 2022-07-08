2dffda84b1de80eef17a7a686d5c94ea100c7192
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-07-08 14:37:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -51,75 +51,76 @@ bot = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
+    plugins={'root': 'AltronBot'},
 )
 
 #-------------------------CLIENTS-----------------------------
 
 if SESSION:
-    client = Client(SESSION, API_ID, API_HASH, plugins=dict(root="AltronUserbot.userbot"))
+    client = Client(SESSION, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
     call_py = PyTgCalls(client)
 else:
     client = None
     call_py = None
 
 if SESSION2:
-    client2 = Client(SESSION2, API_ID, API_HASH, plugins=dict(root="AltronUserbot.userbot"))
+    client2 = Client(SESSION2, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
     call_py2 = PyTgCalls(client2)
 else:
     client2 = None
     call_py2 = None
 
 if SESSION3:
-    client3 = Client(SESSION3, API_ID, API_HASH, plugins=dict(root="AltronUserbot.userbot"))
+    client3 = Client(SESSION3, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
     call_py3 = PyTgCalls(client3)
 else:
     client3 = None
     call_py3 = None
 
 if SESSION4:
-    client4 = Client(SESSION4, API_ID, API_HASH, plugins=dict(root="AltronUserbot.userbot"))
+    client4 = Client(SESSION4, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
     call_py4 = PyTgCalls(client4)
 else:
     client4 = None
     call_py4 = None
 
 if SESSION5:
-    client5 = Client(SESSION5, API_ID, API_HASH, plugins=dict(root="AltronUserbot.userbot"))
+    client5 = Client(SESSION5, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
     call_py5 = PyTgCalls(client5)
 else:
     client5 = None
     call_py5 = None
 
 if SESSION6:
-    client6 = Client(SESSION6, API_ID, API_HASH, plugins=dict(root="AltronUserbot.userbot"))
+    client6 = Client(SESSION6, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
     call_py6 = PyTgCalls(client6)
 else:
     client6 = None
     call_py6 = None
         
 if SESSION7:
-    client7 = Client(SESSION7, API_ID, API_HASH, plugins=dict(root="AltronUserbot.userbot"))
+    client7 = Client(SESSION7, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
     call_py7 = PyTgCalls(client7)
 else:
     client7 = None
     call_py7 = None
 
 if SESSION8:
-    client8 = Client(SESSION8, API_ID, API_HASH, plugins=dict(root="AltronUserbot.userbot"))
+    client8 = Client(SESSION8, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
     call_py8 = PyTgCalls(client8)
 else:
     client8 = None
     call_py8 = None
 
 if SESSION9:
-    client9 = Client(SESSION9, API_ID, API_HASH, plugins=dict(root="AltronUserbot.userbot"))
+    client9 = Client(SESSION9, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
     call_py9 = PyTgCalls(client9)
 else:
     client9 = None
     call_py9 = None
 
 if SESSION10:
-    client10 = Client(SESSION10, API_ID, API_HASH, plugins=dict(root="AltronUserbot.userbot"))
+    client10 = Client(SESSION10, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
     call_py10 = PyTgCalls(client10)
 else:
     client10 = None
