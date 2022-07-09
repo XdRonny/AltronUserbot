bd5591196d971669968b34dacca2ff7aa052cab7
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-07-09 08:48:37 +0530

    Update vcraid.py

--- AltronUserbot/bot/vcraid.py
+++ AltronUserbot/bot/vcraid.py
@@ -33,7 +33,7 @@ async def vcraid(_, e: Message):
     aud = choice(aud_list) 
 
     if inp:
-        bot = await e.reply_text("**Starting Raid**")
+        bot = await e.reply_text("**Starting Raid 😈**")
         link = f"https://github.com/TheAltron{aud[1:]}"
         dl = aud
         songname = aud[18:]
