928c9286635d29d3e75aed4ff2a508e125ceb2f3
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-07-10 19:24:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ if os.path.exists("Internal"):
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-BOT_USERNAME = getenv("BOT_USERNAME", "")
+BOT_USERNAME = getenv("BOT_USERNAME", "User34_bot")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TheAltronX/AltronUserbot")
 SESSION = getenv("SESSION", "")
 SESSION2 = getenv("SESSION2", "")
