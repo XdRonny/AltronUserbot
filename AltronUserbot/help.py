df24edb4f43a0f446723cc1429e37139b93418ef
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-07-11 07:49:58 +0530

    Add files via upload

--- AltronUserbot/help.py
+++ AltronUserbot/help.py
@@ -6,14 +6,9 @@ from pyrogram.types import Message
 @client.on_message(filters.command(["help"], ["/", "!", "."]) & filters.user(SUDO_USERS))
 @client.on_message(filters.command(["help"], ["/", "!", "."]) & filters.me)
 async def start(Client, message: Message):
-    HOME_TEXT = """
-**ʜᴇʏᴀ..!! 💖**
-✘ __ɪ'ᴍ ɴᴏᴛ ᴊᴜsᴛ ᴀɴ ᴜsᴇʀʙᴏᴛ, ɪ ᴀᴍ ᴀʟsᴏ ᴀ sᴘᴀᴍʙᴏᴛ ʙᴏᴛ. ɪ ᴄᴀɴ ᴘʟᴀʏ ᴍᴜsɪᴄ ᴛʜʀᴏᴡ ᴜsᴇʀʙᴏᴛ. ɪ ᴄᴀɴ sᴘᴀᴍ ᴍsɢ ғʀᴏᴍ ʙᴏᴛ ᴀɴᴅ ᴜsᴇʀʙᴏᴛ ʙᴏᴛʜ. ɪ ʜᴀᴠᴇ ʟᴏᴛꜱ ᴏꜰ ꜰᴇᴀᴛᴜʀᴇꜱ ᴡʜɪᴄʜ ʏᴏᴜ ʟɪᴋᴇꜱ ᴛʜᴀᴛ__.
-
-✘ [ᴄʟɪᴄᴋ ᴏɴ ʜᴇʀᴇ ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ ℹ️](https://t.me/{BOT_USERNAME})
-"""
-  
-    await message.reply_text(HOME_TEXT)
+    await message.reply_text(
+    "**ʜᴇʏᴀ..!! **\n\n[✘ ᴄʟɪᴄᴋ ᴏɴ ʜᴇʀᴇ ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ ℹ️](https://t.me/{BOT_USERNAME}?start=true)"
+    )
     
     
 
