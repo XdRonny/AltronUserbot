6089f641d42663224a7efd1324f42a06d0d2e1ff
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-07-11 08:08:26 +0530

    Add files via upload

---
+++ AltronUserbot/bot/userbot-help.py
@@ -0,0 +1,14 @@
+from pyrogram import filters, Client
+from config import BOT_USERNAME, client, SUDO_USERS
+from pyrogram.types import Message
+
+
+@client.on_message(filters.command(["help"], ["/", "!", "."]) & filters.user(SUDO_USERS))
+@client.on_message(filters.command(["help"], ["/", "!", "."]) & filters.me)
+async def start(Client, message: Message):
+    await message.reply_text(
+    "**ʜᴇʏᴀ..!! **\n\n» __ғᴏʀ ʜᴇʟᴘ ᴍᴇɴᴜ ɢᴏ ᴛᴏ ʏᴏᴜʀ ᴄʀᴇᴀᴛᴇᴅ ʙᴏᴛ's ᴅᴍ__ » @{BOT_USERNAME}\n\n__sᴏᴏɴ ᴀᴅᴅɪɴɢ ɪɴʟɪɴᴇ ʜᴇʟᴘ ᴍᴇɴᴜ ʙᴜᴛᴛᴏɴs ɪɴ ᴜsᴇʀʙᴏᴛ__ ᴊᴏɪɴ » @Altron_X"
+    )
+    
+    
+
